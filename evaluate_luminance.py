7ea2610d8e0c58da52498102245234acf14a915d
yyang <yidong.yang@isc.cnrs.fr>
2026-01-29 14:34:11 +0100

    Evaluate luminance

---
+++ evaluate_luminance.py
@@ -0,0 +1,68 @@
+# This script was used to compute the perceived luminance (accounting for how human eyes perceive brightness differently for different colors)
+from PIL import Image
+from pathlib import Path
+import re
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def natural_key(s):
+    """
+    Split a string into chunks of numbers and non-numbers,
+    to allow natural sorting (e.g., 1,2,3,10 instead of 1,10,2,3).
+    Example: 'image10.png' -> ['image', 10, '.png']
+    """
+    parts = re.split(r'(\d+)', s)
+    return [int(p) if p.isdigit() else p.lower() for p in parts]
+
+in_dir = Path("Images/")
+exts = {".jpg", ".jpeg"}
+# sort the files in human-friendly order
+files = [p for p in in_dir.rglob("*") if p.suffix.lower() in exts]
+files.sort(key=lambda p: natural_key(p.name))
+
+# compute the luminance of each image
+luminance_list = []
+for idx, p in enumerate(files, start=1):
+
+    # open image
+    img = Image.open(p)
+    rgb = np.array(img.convert('RGB'))
+
+    # Weighted luminance formula (ITU-R BT.601)
+    luminance = 0.299 * rgb[:, :, 0] + 0.587 * rgb[:, :, 1] + 0.114 * rgb[:, :, 2]
+
+    avg_luminance = luminance.mean()
+    luminance_list.append([idx, p, avg_luminance])
+
+
+luminance_df = pd.DataFrame(data=luminance_list, columns=["ImageNumber", "ImageName", "Luminance"])
+luminance_df.to_csv("Image_luminance.csv", index=False)
+
+# evaluate the effect of category on luminance
+# load the experimental data
+df_img = pd.read_csv("Data_exp1.csv",usecols=["ImageNumber", "Category"]).drop_duplicates()
+df_luminance = pd.merge(left=df_img, right=luminance_df, on="ImageNumber")
+
+
+# Plots
+import seaborn as sns
+plt.figure(figsize=(8,7))
+sns.boxplot(x="Category", y="Luminance", data=df_luminance)
+sns.stripplot(x="Category", y="Luminance", data=df_luminance, color="black", size=3, alpha=0.5)
+plt.title("Perceived luminance by Category")
+plt.xticks(rotation=45)
+plt.show()
+
+
+
+# ANOVA
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+# one-way ANOVA
+model = ols("Luminance ~ C(Category)", data=df_luminance).fit()
+anova_table = sm.stats.anova_lm(model, typ=2)
+print("=== One-way ANOVA ===")
+print(anova_table)
